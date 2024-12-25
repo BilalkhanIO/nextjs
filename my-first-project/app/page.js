eb4efe1e2e88959cd7b8927ab2aa11cd9b17b2a6
bilal khan <48455259+BilalkhanIO@users.noreply.github.com>
2024-12-25 14:27:21 +0000

    starting project

---
+++ my-first-project/app/page.js
@@ -0,0 +1,9 @@
+export default function Home(){
+  return (
+    <main>
+      <img src="/favicon.ico" alt="A server surrended by magic sparkles."/>
+      <h1>Wellcome to this nextJS project</h1>
+      <p>let&apos;s get started!</p>
+    </main>
+  )
+}
